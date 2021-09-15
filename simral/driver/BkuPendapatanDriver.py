c202ecf8be16ac6b52f786ddb56792fd3fcecf0f
satriaprayoga <satria.prayoga@gmail.com>
2021-09-15 13:21:59 +0700

    bku pendapatan

--- simral/driver/BkuPendapatanDriver.py
+++ simral/driver/BkuPendapatanDriver.py
@@ -47,30 +47,30 @@ class BkuPendapatanDriver(SimralDriver):
             self._driver.find_element_by_id("tgl_trx").clear()
 
             self._driver.find_element_by_id("tgl_trx").send_keys(data['tgl_transaksi'])
-
+            self._driver.implicitly_wait(1)
             self._driver.find_element_by_id("uraian_trx").send_keys(data['uraian'].strip())
-
+            self._driver.implicitly_wait(1)
             self._driver.find_element_by_id("no_bukti").send_keys(data['no_bukti'].strip())
-
+            self._driver.implicitly_wait(1)
             self._driver.execute_script("""
                 document.getElementById('cara_pembayaran').style.display = "inline";
 
                 """)
             Select(self._driver.find_element_by_id("cara_pembayaran")).select_by_visible_text(data['cara_pembayaran'].strip())
-
+            self._driver.implicitly_wait(1)
 
             self._driver.execute_script("""
                 document.getElementById('pendapatan_thn_lalu').style.display = "inline";
 
                 """)
             Select(self._driver.find_element_by_id("pendapatan_thn_lalu")).select_by_visible_text(data['status_pendapatan'].strip())
-
+            self._driver.implicitly_wait(1)
             self._driver.execute_script("""
                 document.getElementById('sikd_skpkd_bank_account_id').style.display = "inline";
 
                 """)
             Select(self._driver.find_element_by_id("sikd_skpkd_bank_account_id")).select_by_visible_text(data['rekening_setoran'])
-
+            self._driver.implicitly_wait(1)
             dpa_id=self._driver.find_element_by_id('dpa_dpa_id')
             WebDriverWait(self._driver, 5).until(expected_conditions.staleness_of(dpa_id))
             dpa_id=WebDriverWait(self._driver, 5).until(expected_conditions.presence_of_element_located((By.ID,'dpa_dpa_id')))
@@ -79,23 +79,23 @@ class BkuPendapatanDriver(SimralDriver):
                  Select(dpa_id).select_by_visible_text(data["nama_dpa"])
             else:
                 Select(dpa_id).select_by_visible_text(f'[{data["no_dpa"]}] {data["nama_dpa"]}')
-
+            self._driver.implicitly_wait(1)
             self._driver.execute_script("""
                 document.getElementById('sikd_rek_jenis_id').style.display = "inline";
 
                 """)
             Select(self._driver.find_element_by_id("sikd_rek_jenis_id")).select_by_visible_text(f'[{data["rekening_jenis"]}] {data["nama_jenis"]}')
-        
+            self._driver.implicitly_wait(1)
             rek_obj=self._driver.find_element_by_id('sikd_rek_obj_id')
             WebDriverWait(self._driver, 5).until(expected_conditions.staleness_of(rek_obj))
             rek_obj=WebDriverWait(self._driver, 5).until(expected_conditions.presence_of_element_located((By.ID,'sikd_rek_obj_id')))
             Select(rek_obj).select_by_visible_text(f'[{data["rekening_objek"]}] {data["nama_objek"]}')
-
+            self._driver.implicitly_wait(1)
             rek_rinc_obj=self._driver.find_element_by_id('sikd_rek_rincian_obj_id')
             WebDriverWait(self._driver, 5).until(expected_conditions.staleness_of(rek_rinc_obj))
             rek_rinc_obj=WebDriverWait(self._driver, 5).until(expected_conditions.presence_of_element_located((By.ID,'sikd_rek_rincian_obj_id')))
             Select(rek_rinc_obj).select_by_visible_text(f'[{data["rekening_rincian"]}] {data["nama_rincian"]}')
-
+            self._driver.implicitly_wait(1)
             jumlah=self._driver.find_element_by_id('jumlah_0')
             WebDriverWait(self._driver, 5).until(expected_conditions.staleness_of(jumlah))
             jumlah=WebDriverWait(self._driver, 5).until(expected_conditions.presence_of_element_located((By.ID,'jumlah_0')))
